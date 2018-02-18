b6e26aa056a57ed73a39fd383c9611d5e67e1c78
Jonathan Xu <jonathanlikestoemail@gmail.com>
2018-02-18 09:56:57 -0500

    projec update

--- project/project.ino
+++ project/project.ino
@@ -18,20 +18,27 @@ void setup() {
   Serial.begin(9600);
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
+  servo3.attach(6);
+  servo4.attach(7);
+  servo3.write(80);
+  servo4.write(100);
+  delay(500);
+
   servo1.attach(3);
   servo2.attach(5);
+  servo1.write(0);
+  servo2.write(0);
+  delay(500);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i; i <= 90; i += 5){
-    servo3.attach(6);
-    servo4.attach(11);
+  for (int i = 90; i >= 0; i -= 5){
+
     servo3.write(180-i);
-    servo4.write(i+5);
+    servo4.write(i);
+    
     delay(500);
-    servo3.detach();
-    servo4.detach();
     
     if (turn%2 == 0){
       for (int x = 0; x <= 180; x += 5){
@@ -40,7 +47,7 @@ void loop() {
         Serial.print(',');
         Serial.print(2*x); // Base position in degrees
         Serial.print(',');
-        Serial.println(i); // Side agle position in degrees
+        Serial.println(i-10); // Side agle position in degrees
         servo1.write(x);
         servo2.write(x);
         delay(150);
@@ -58,9 +65,7 @@ void loop() {
         servo2.write(x);
         delay(150);
       }
-    }
-    turn ++;
-   
+    }   
   }
   
 
