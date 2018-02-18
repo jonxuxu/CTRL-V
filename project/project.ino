96fc96e2d61e94bd3a96dde14c60a9842f311ae8
Jonathan Xu <jonathanlikestoemail@gmail.com>
2018-02-18 09:11:25 -0500

    Works

--- project/project.ino
+++ project/project.ino
@@ -4,7 +4,10 @@ const int trig = 9;
 const int echo = 10;
 Servo servo1;
 Servo servo2;
+Servo servo3;
+Servo servo4;
 int pos = 0;
+int turn = 0;
 
 long duration;
 int distance;
@@ -21,24 +24,45 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int x = 0; x <= 180; x ++){
-    cm = ping();
-    Serial.print(cm); // Distance in cm
-    Serial.print(',');
-    Serial.println(2*x); // Base position in degrees
-    servo1.write(x);
-    servo2.write(x);
-    delay(15);
+  for (int i; i <= 90; i += 5){
+    servo3.attach(6);
+    servo4.attach(11);
+    servo3.write(180-i);
+    servo4.write(i+5);
+    delay(500);
+    servo3.detach();
+    servo4.detach();
+    
+    if (turn%2 == 0){
+      for (int x = 0; x <= 180; x += 5){
+        cm = ping();
+        Serial.print(cm); // Distance in cm
+        Serial.print(',');
+        Serial.print(2*x); // Base position in degrees
+        Serial.print(',');
+        Serial.println(i); // Side agle position in degrees
+        servo1.write(x);
+        servo2.write(x);
+        delay(150);
+      }
+    }
+    else {   
+      for (int x = 180; x >= 0; x -= 5){
+        cm = ping();
+        Serial.print(cm); // Distance in cm
+        Serial.print(',');
+        Serial.print(2*x); // Base position in degrees
+        Serial.print(',');
+        Serial.println(i); // Side agle position in degrees
+        servo1.write(x);
+        servo2.write(x);
+        delay(150);
+      }
+    }
+    turn ++;
+   
   }
-  for (int x = 180; x >= 0; x --){
-    cm = ping();
-    Serial.print(cm); // Distance in cm
-    Serial.print(',');
-    Serial.println(2*x); // Base position in degrees
-    servo1.write(x);
-    servo2.write(x);
-    delay(15);
-  }
+  
 
   Serial.println("End");
   
