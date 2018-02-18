7fa93255c5b3dba5113973dbec28305cb9b08212
Farwhyn <jamesizhou@gmail.com>
2018-02-18 00:38:05 -0500

    added firebase integration

--- read.py
+++ read.py
@@ -1,24 +1,34 @@
 # Reads data from Arduino and saves to file
-
 import serial
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyDrr76124iABn9Ukw0NgmXFfdnnW_92thg",
+  "authDomain": "threedscanner.firebaseapp.com",
+  "databaseURL": "https://threedscanner.firebaseio.com",
+  "storageBucket": "threedscanner.appspot.com",
+}
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
+
 ser = serial.Serial("COM3", 9600)
 data = []
 
-with open("data.txt", "w") as f:
-    flag = True
-    while (flag):
-        distance = ser.readline().decode().strip('\r\n')
-        print(distance)
-        if ("End" in distance):
-            print("im exiting")
-            flag = False
 
-        data.append(distance.split(','))
-    data.pop(0)
-    data = data[:-1]
-    print(data)
-    f.write(data)
-    f.flush()
+flag = True
+while (flag):
+    distance = ser.readline().decode().strip('\r\n')
+
+    if ("End" in distance):
+        flag = False
+
+    data.append(distance.split(','))
+data.pop(0)
+data = data[:-1]
 
 print("Finished")
 ser.close()
+
+db.child('users').push(data)
+
